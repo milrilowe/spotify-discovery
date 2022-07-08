dca48bbb2cdc856215d538786f44ffa896721117
Miles Owens <milesrhowens@gmail.com>
2022-07-08 00:13:33 -0700

    heroku url

--- src/helperFunctions/recommendedTracks.js
+++ src/helperFunctions/recommendedTracks.js
@@ -3,7 +3,7 @@ const refreshToken = require("./refreshToken.js");
 
 const recommendedTracks = async (currentSong, access_token) => {
 
-    let url = `http://localhost:3000/recommendations/${access_token}`;
+    let url = `https://spotify-discover-music.herokuapp.com/${access_token}`;
     let data = await makeRequest("POST", url, currentSong.id);
     return data;
     
